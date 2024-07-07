a3d421bf8b70bfdcfe1e9f4863ac3a7e488c9282
shubham <gavadeshubham2004@gmail.com>
2024-07-07 13:18:04 +0530

    modified:   lib/login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -158,7 +158,7 @@ class _LoginState extends State<Login> {
                 ),
                 SizedBox(height: 20),
                 Text('Or continue with'),
-                SizedBox(height: 30),
+                SizedBox(height: 20),
                 GestureDetector(
                   onTap: () => loginWithGoogle(),
                   child: Container(
